8c966368bd888e6df75a3e34909f7670577c2ed5
Nibin Geevarghese <ngeevarghese@qualys.com>
2021-06-10 06:48:44 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,12 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/ngeevarghese-training/s3-bucket/aws"
+  version = "2.2.0"
 
   bucket = "my-s3-bucket"
   acl    = "private"
 
   bucket_prefix = "ngeevarghese"
-  
+
   versioning = {
     enabled = true
   }
